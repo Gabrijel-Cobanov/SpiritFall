9779e556cb4b184beeaa32b4fa68f8008225aed7
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-06-06 18:24:13 +0200

    Made the Totem interactible
    All that is left to do is make the totem read and write to the save file and we are good to go, also make the nooks where they are hidden a little taller

--- Scripts/Environment behavior Scripts/TotemBehavior.gd
+++ Scripts/Environment behavior Scripts/TotemBehavior.gd
@@ -3,21 +3,37 @@ extends Sprite2D
 @export var totem_id: int
 
 @onready var area_2d = $Area2D
-@onready var selected = $CollectibleStoneSelected
+@onready var stone_selected = $CollectibleStoneSelected
+
 
 @onready var animation_player = $AnimationPlayer
 
-var should_be_awake: bool = false
+var is_awake: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	area_2d.body_entered.connect(func(body): selected.visible = true)
-	area_2d.body_exited.connect(func(body): selected.visible = false)
 	
-	if !should_be_awake:
+	stone_selected.visible = false
+	
+	area_2d.body_entered.connect(func(body): if !is_awake: stone_selected.visible = true)
+	area_2d.body_exited.connect(func(body): stone_selected.visible = false)
+	
+	if !is_awake:
 		animation_player.play("idle")
 	else:
 		animation_player.play("awake")
+		
+func _process(delta):
+	if stone_selected.visible and Input.is_action_just_pressed("interact") and !is_awake:
+		stone_selected.visible = false
+		is_awake = true
+		animation_player.play("waking")
+		await animation_player.animation_finished
+		animation_player.play("launching")
+		await animation_player.animation_finished
+		animation_player.play("awake")
+
+		
 	
 	# read from the savefile whether this stone should be active
 	# find the id, if it's flipped, turn on the stone when the level is first loaded and
