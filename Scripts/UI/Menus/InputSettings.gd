1514768e3767247d958800e6507bfa52dd013831
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-06-15 17:13:41 +0200

    Started work on the input settings menu
    Next step is making the buttons grab focus and making them neighbors dynamically

---
+++ Scripts/UI/Menus/InputSettings.gd
@@ -0,0 +1,50 @@
+extends Control
+
+
+@onready var input_button = preload("res://Scenes/Menus/InputMenu/InputButton.tscn")
+@onready var action_list = $Panel/MarginContainer/VBoxContainer/ScrollContainer/ActionList
+
+
+var is_remapping: bool = false
+var action_to_remap = null
+var remapping_button = null
+
+var input_actions = {
+	"up" : "Move up",
+	"down" : "Move down",
+	"left" : "Move left",
+	"right" : "Move right",
+	"jump" : "Jump",
+	"attack" : "Attack",
+	"dash" : "Dash",
+	"heal" : "Heal",
+	"interact" : "Interact"
+}
+
+
+func _ready():
+	Create_Action_List()
+	
+
+func Create_Action_List():
+	InputMap.load_from_project_settings()
+	for item in action_list.get_children():
+		item.queue_free()
+		
+	for action in input_actions:
+		var button = input_button.instantiate()
+		var action_label = button.find_child("LabelAction")
+		var input_label = button.find_child("LabelInput")
+		
+		action_label.text = input_actions[action]
+		
+		var events = InputMap.action_get_events(action)
+		if events.size() > 0:
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
+		else:
+			input_label.text = ""
+			
+		action_list.add_child(button)
+	var action_buttons = action_list.get_children()
+
+
--- Scripts/UI/Menus/MainMenu.gd
+++ Scripts/UI/Menus/MainMenu.gd
@@ -1,12 +1,17 @@
 extends Control
 
+@onready var logo = $TextureRect
+@onready var buttons = $Buttons
 @onready var start_button: Button = $Buttons/Start
+@onready var options_button = $Buttons/Options
 @onready var exit_button = $Buttons/Exit
+@onready var input_settings = $InputSettings
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	start_button.grab_focus()
 	start_button.pressed.connect(On_Start_Button_Pressed)
+	options_button.pressed.connect(Toggle_Elements_Visibility)
 	exit_button.pressed.connect(On_Exit_Pressed)
 	
 	#Upon first start up, make a game_data file, only one save file, to not complicate things
@@ -20,6 +25,11 @@ func _ready():
 		SaveSystem.save()
 	else:
 		print("Data was not null, loaded this %s", [game_data])
+		
+func _process(delta):
+	if input_settings.visible and Input.is_action_just_pressed("pause_game"):
+		Toggle_Elements_Visibility()
+		start_button.grab_focus()
 	
 func On_Start_Button_Pressed():
 	SceneTransitionManager.Transition_To_Scene("res://Scenes/Levels/HubWorld.tscn")
@@ -29,3 +39,10 @@ func On_Exit_Pressed():
 	SaveSystem.set_var("save_file_1", game_data)
 	SaveSystem.save()
 	get_tree().quit()
+	
+func Toggle_Elements_Visibility():
+	logo.visible = !logo.visible
+	buttons.visible = !buttons.visible
+	input_settings.visible = !input_settings.visible
+	
+	
