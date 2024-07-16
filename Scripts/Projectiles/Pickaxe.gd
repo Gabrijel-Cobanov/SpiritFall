06af157a84ecef469b4f93008c8d593e257b6a01
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-07-16 20:59:46 +0200

    Ditched the bezier curve idea, I don't know enough about it to make it work right
    Made the projectile launcehr launch a projectile independently of the bot that threw it, made standalone styles for buttons, so that the UI looks consistent and pretty, not much but it is work

--- Scripts/Projectiles/Pickaxe.gd
+++ Scripts/Projectiles/Pickaxe.gd
@@ -1,22 +1,11 @@
-extends Path2D
+extends CharacterBody2D
 
-@export var speed: int = 100
-@export var target_position: Vector2
+@onready var animation_player = $AnimationPlayer
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animation_player = $PathFollow2D/Area2D/AnimationPlayer
-@onready var path_follow_2d = $PathFollow2D
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play("falling")
-	curve.set_point_out(0, Vector2(target_position.x/2, -abs(target_position.x)))
-	curve.set_point_position(1, target_position)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if not target_position: return
-		
-	if path_follow_2d.progress_ratio >= 0.98: queue_free()
 	
-	path_follow_2d.progress += speed * delta
+func _physics_process(delta):
+	move_and_slide()
+
--- Scripts/Projectiles/ProjectileShooter.gd
+++ Scripts/Projectiles/ProjectileShooter.gd
@@ -1,22 +1,11 @@
 extends Node2D
 
-@export var arcing: bool = true
-var arc_curve = 100
-
-@export var end_marker: Marker2D
+var target_position: Vector2
 const PICKAXE = preload("res://Scenes/Projectiles/Pickaxe.tscn")
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		Shoot()
-
-func Shoot():
-	var start_pos = end_marker.global_position
-	var direction = 1
-	var target_pos = Vector2(start_pos.x, start_pos.y) - global_position
-	
+func Shoot_Arc():
 	var pickaxe = PICKAXE.instantiate()
-	pickaxe.target_position = target_pos
+	pickaxe.velocity = Vector2(1, 1)
 	
 	pickaxe.global_position = global_position
 	get_parent().add_child(pickaxe)
