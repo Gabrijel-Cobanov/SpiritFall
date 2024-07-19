6fa28f4e76be544fd068fae1e5b8dd9eb779346c
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-07-19 18:22:34 +0200

    Finished the firefly behavior

--- Scripts/Environment behavior Scripts/FireflyBehavior.gd
+++ Scripts/Environment behavior Scripts/FireflyBehavior.gd
@@ -1,8 +1,41 @@
-extends CharacterBody2D
+extends Sprite2D
 
-var floating_speed: int = 2
+var floating_speed: float
+const MAX_FLOAT: float = 1.3
 
-func _physics_process(delta):
+var starting_position: Vector2
+var current_position: Vector2
+
+var floating_direction: Vector2
+var floating_up: bool
+
+func _ready():
+	starting_position = global_position
+	current_position = starting_position
+	var direction = randi()
+	if direction % 2 == 0:
+		floating_direction = Vector2.UP
+		floating_up = true
+	else:
+		floating_direction = Vector2.DOWN
+		floating_up = false
+	floating_speed = randf()/10
 	
 
-	move_and_slide()
+func _physics_process(delta):
+	Float_Up_And_Down()
+
+
+func Float_Up_And_Down():
+	global_position += floating_direction * floating_speed
+	current_position = global_position
+	if current_position.distance_to(starting_position) > MAX_FLOAT:
+		Switch_Floating_Direction()
+		
+
+func Switch_Floating_Direction():
+	if floating_up:
+		floating_direction = Vector2.DOWN
+	else:
+		floating_direction = Vector2.UP
+	floating_up = !floating_up
