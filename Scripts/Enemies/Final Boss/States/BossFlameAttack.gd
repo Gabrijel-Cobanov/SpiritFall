ee76363c0679501a008d147c7c2c72a3795a4751
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2024-08-15 16:50:00 +0200

    Made the final boss animations and state classes

---
+++ Scripts/Enemies/Final Boss/States/BossFlameAttack.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name BossFlameMiddle
+
+func Enter(ctx: BossStateMachine):
+	pass
+
+func Update(ctx: BossStateMachine):
+	pass
+	
+func Pyhsics_Update(ctx: BossStateMachine, delta: float):
+	pass
+	
+func Exit(ctx: BossStateMachine):
+	pass
